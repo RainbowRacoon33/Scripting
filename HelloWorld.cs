946cd8c0a6b2d75fda5d785972a0bc624d712377
RainbowRacoon_Dev <151455330+RainbowRacoon33@users.noreply.github.com>
2026-01-20 16:57:40 +0100

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    void Start()
+    {   
+        // Hola :)
+        print("Hello World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
